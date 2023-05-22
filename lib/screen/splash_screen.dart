a62999795c7fb911bf917ca533b08f9ea32024cd
Aditia Surya Putra <kurohiko06@gmail.com>
2023-05-23 00:47:29 +0700

    Add global color,logo

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecomplaint/config/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Primary"),
+            Text(style: TextStyle(color: AppColors.fontsecondary), "Percobaan"),
+            FilledButton(onPressed: () {}, child: const Text("primary")),
+            ElevatedButton(onPressed: () {}, child: const Text("primary")),
+            Image.asset(""),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class IntroductionScreen extends StatefulWidget {
+  const IntroductionScreen({Key? key}) : super(key: key);
+
+  @override
+  _IntroductionScreenState createState() => _IntroductionScreenState();
+}
+
+class _IntroductionScreenState extends State<IntroductionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
