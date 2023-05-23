110d8de4860bf49aee47e8edf026543694cd7007
Aditia Surya Putra <kurohiko06@gmail.com>
2023-05-23 19:51:47 +0700

    update splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecomplaint/config/colors.dart';
 import 'package:flutter_ecomplaint/screen/homepage.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -21,7 +24,7 @@ class _SplashScreenState extends State<SplashScreen> {
             //splash screen won't show on back button press
             //navigation to Home page.
             builder: (context) {
-          return IntroductionScreen();
+          return OnBoardingPage();
         }));
       } else {
         Navigator.pushReplacement(context, MaterialPageRoute(
@@ -52,23 +55,96 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
-class IntroductionScreen extends StatefulWidget {
-  const IntroductionScreen({Key? key}) : super(key: key);
+class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
 
   @override
-  _IntroductionScreenState createState() => _IntroductionScreenState();
+  OnBoardingPageState createState() => OnBoardingPageState();
 }
 
-class _IntroductionScreenState extends State<IntroductionScreen> {
+class OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/$assetName', width: width);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Container(
-          child: Image.asset(
-            'assets/logo/icon-logo.png',
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      allowImplicitScrolling: true,
+      autoScrollDuration: 3000,
+
+      pages: [
+        PageViewModel(
+          title: ".",
+          bodyWidget: _buildImage(
+            'logo/logo-splash-screen.png',
           ),
+          decoration: const PageDecoration(
+            titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700, color: Colors.white),
+            bodyTextStyle: bodyStyle,
+            bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+            pageColor: Colors.white,
+            imagePadding: EdgeInsets.zero,
+            titlePadding: EdgeInsets.only(top: 100),
+          ),
+        ),
+        PageViewModel(
+          title: "Learn as you go",
+          body: "Download the Stockpile app and master the market with our mini-lesson.",
+          image: _buildImage('img2.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Kids and teens",
+          body: "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+          image: _buildImage('img3.jpg'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => Navigator.of(context).push(
+        MaterialPageRoute(builder: (_) => const Homepage()),
+      ),
+      onSkip: () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => const Homepage()),
+      ),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: false,
+      //rtl: true, // Display as right-to-left
+      back: const Icon(Icons.arrow_back),
+      skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
+      next: const Icon(Icons.arrow_forward),
+      done: const Icon(Icons.arrow_forward),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb ? const EdgeInsets.all(12.0) : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
         ),
       ),
     );
